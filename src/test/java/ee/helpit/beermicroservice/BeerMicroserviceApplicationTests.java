defb06c4c1f87e7aeaa550a5c554905f68a2e482
Henry Aimre <henryaimre@gmail.com>
2019-08-03 21:22:37 +0300

    Initial empty commit

---
+++ src/test/java/ee/helpit/beermicroservice/BeerMicroserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.helpit.beermicroservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeerMicroserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
